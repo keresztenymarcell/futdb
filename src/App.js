293a8c9603be6d81033e611bf098dff7d56782e9
keresztenymarcell <kereszteny.marcell@gmail.com>
2022-04-27 19:15:56 +0200

    add basic routes to app and create files needed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Error from './pages/Error';
+import PlayerPage from './pages/PlayerPage';
+
 
 function App() {
   return (
-    <>
-    </>
+    <Router>
+        <Routes>
+            <Route exact path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/player/:id' element={<PlayerPage/>} />
+            <Route path='*' element={<Error />} />
+        </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/PlayerPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PlayerPage = () => {
+  return (
+    <div>PlayerPage</div>
+  )
+}
+
+export default PlayerPage
