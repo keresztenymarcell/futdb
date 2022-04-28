54507492d1e44796305f637a6d159857c318c36b
keresztenymarcell <kereszteny.marcell@gmail.com>
2022-04-28 11:53:48 +0200

    refact: rename used component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Error from './pages/Error';
-import PlayerPage from './pages/PlayerPage';
+import Navbar from './components/Navbar';
+import SingleMeal from './pages/SingleMeal';
 
 
 function App() {
   return (
     <Router>
+        <Navbar />
         <Routes>
             <Route exact path='/' element={<Home />} />
             <Route path='/about' element={<About />} />
-            <Route path='/player/:id' element={<PlayerPage/>} />
+            <Route path='/meal/:id' element={<SingleMeal/>} />
             <Route path='*' element={<Error />} />
         </Routes>
     </Router>
